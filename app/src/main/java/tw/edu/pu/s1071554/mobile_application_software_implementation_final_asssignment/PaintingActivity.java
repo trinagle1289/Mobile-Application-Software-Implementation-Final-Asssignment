ac4000cd5a8ceb0387dc0a5f4aebdaa090e15076
trinagle1289 <denny95090@gmail.com>
2022-06-04 15:41:53 +0800

    [feat] 完成主介面和繪圖介面的畫面

---
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_application_software_implementation_final_asssignment/PaintingActivity.java
@@ -0,0 +1,14 @@
+package tw.edu.pu.s1071554.mobile_application_software_implementation_final_asssignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaintingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_painting);
+    }
+}
