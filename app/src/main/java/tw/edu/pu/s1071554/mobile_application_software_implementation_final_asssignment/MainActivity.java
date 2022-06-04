a610c4bc8e19009525dd1440e51bddc8897a399e
trinagle1289 <denny95090@gmail.com>
2022-06-04 16:06:58 +0800

    [feat] 完成主介面和繪圖介面的功能

--- app/src/main/java/tw/edu/pu/s1071554/mobile_application_software_implementation_final_asssignment/MainActivity.java
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_application_software_implementation_final_asssignment/MainActivity.java
@@ -2,13 +2,34 @@ package tw.edu.pu.s1071554.mobile_application_software_implementation_final_asss
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btStart = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btStart = findViewById(R.id.main_btStart);
+        setButton();
     }
+
+    private void setButton() {
+
+        btStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, PaintingActivity.class);
+                startActivity(it);
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/tw/edu/pu/s1071554/mobile_application_software_implementation_final_asssignment/PaintingActivity.java
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_application_software_implementation_final_asssignment/PaintingActivity.java
@@ -2,13 +2,80 @@ package tw.edu.pu.s1071554.mobile_application_software_implementation_final_asss
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PaintingActivity extends AppCompatActivity {
+
+    Button btWall = null;
+    Button btDoor = null;
+    Button btTable = null;
+    Button btBack = null;
+    Button btErase = null;
+    Button btSave = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painting);
+
+        btWall = findViewById(R.id.painting_btWall);
+        btDoor = findViewById(R.id.painting_btDoor);
+        btTable = findViewById(R.id.painting_btTable);
+        btBack = findViewById(R.id.painting_btBack);
+        btErase = findViewById(R.id.painting_btErase);
+        btSave = findViewById(R.id.painting_btSave);
+
+        setButton();
+
     }
+
+    private void setButton() {
+        btWall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btDoor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btTable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btErase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
 }
